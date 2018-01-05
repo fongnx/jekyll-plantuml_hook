9c864ad2ba56ac1d35b33e81abecdcfec9188aa4
Chulki Lee <chulki.lee@gmail.com>
2018-01-05 13:04:14 -0800

    initial commit

---
+++ lib/jekyll/plantuml_hook.rb
@@ -0,0 +1,7 @@
+require "jekyll/plantuml_hook/version"
+
+module Jekyll
+  module PlantumlHook
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/plantuml_hook/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module PlantumlHook
+    VERSION = "0.1.0"
+  end
+end
